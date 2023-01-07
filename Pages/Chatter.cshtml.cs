e2784cf32046a5fcf315ae1e785c87e1c39f122c
Your Name <you@example.com>
2023-01-07 11:28:07 -0800

    ported project from react to .net

---
+++ Pages/Chatter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioDotNet.Pages
+{
+    public class ChatterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
